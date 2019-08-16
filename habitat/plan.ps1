06e5e851044b4f2e0d6b414ca5890caf13135d9e
Patrick Schaumburg <pschaumburg@tecracer.de>
2019-08-16 11:44:55 +0200

    fix bug and use cache path instead of project path

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -4,6 +4,8 @@ $pkg_maintainer="Patrick Schaumburg <info@p-schaumburg.de>"
 $pkg_description="Putty Client"
 $pkg_version='0.72'
 $pkg_license=('Apache-2.0')
+$pkg_bin_dirs=('bin')
+$package_source = "https://the.earth.li/~sgtatham/putty/${pkg_version}/w32/putty.exe"
 
 function Invoke-BeforeDefault {
   do_default_before
@@ -12,9 +14,8 @@ function Invoke-BeforeDefault {
 
 function Invoke-Download {
   New-Item -ItemType Directory -Force -Path ${PLAN_CONTEXT}/../bin/
-  $package_source = "https://the.earth.li/~sgtatham/putty/${pkg_version}/w32/putty.exe"
   $Session = New-Object -TypeName Microsoft.PowerShell.Commands.WebRequestSession
-  Invoke-WebRequest -Uri $package_source -WebSession $Session -OutFile ${PLAN_CONTEXT}/../bin/${pkg_name}-${pkg_version}.exe
+  Invoke-WebRequest -Uri $package_source -WebSession $Session -OutFile $HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}.exe
 }
 
 function Invoke-Build {
@@ -22,6 +23,6 @@ function Invoke-Build {
 }
 
 function Invoke-Install {
-  Copy-Item ${PLAN_CONTEXT}/../bin/${pkg_name}-${pkg_version}.exe ${pkg_prefix}/ -Recurse -Force
+  Copy-Item $HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}.exe ${pkg_prefix}/bin -Recurse -Force
 }
 
